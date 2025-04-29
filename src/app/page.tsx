28d755dc006a34ebae17ac8e77898151f75d9e55
srikanth <srikanth@digiwebspot.com>
2025-04-29 17:01:07 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,10 @@ import { useTranslations } from "next-intl";
 
 export default function Home() {
   const t = useTranslations("HomePage")
+  const th = useTranslations("TabTitle");
   return (
     <div>
-      <h3 className="text-3xl font-bold text-blue text-center mt-8">Light and Dark Mode</h3>
+      <h3 className="text-3xl font-bold text-blue text-center mt-8">{th("home")}</h3>
       <div className="flex justify-center mt-4">
         <ModeToggle />
       </div>
